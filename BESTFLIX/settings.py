b05f95b48653eef8695a42b135ea9b5d8e0e3a61
mmelihdogan <doganmmelih@gmail.com>
2021-01-19 19:51:47 +0300

    others-you can delete it bro

--- BESTFLIX/settings.py
+++ BESTFLIX/settings.py
@@ -78,8 +78,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'USER': 'bestflix',
+        'PASSWORD': 'development',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
